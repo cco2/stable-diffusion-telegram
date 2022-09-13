29fff9b7f90c35b6b06ee9fcd785507e90011c5e
Chris <pickles133@gmail.com>
2022-09-12 22:08:45 -0700

    Bot now reads prompt and replies back with image

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
+import glob
 import telebot
 from dotenv import load_dotenv
+
+TXT2IMG = "../stable-diffusion/scripts/txt2img.py "
+PROMPTARG = "--prompt "
+OTHERARG = "--plms --n_iter 1 --n_samples 1 --skip_grid --seed -1"
 
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
@@ -10,8 +15,16 @@ bot = telebot.TeleBot(API_KEY)
 def greet(message):
     bot.reply_to(message, "Hey! How's it going?")
 
-@bot.message_handler(commands=['hello'])
+def send_prompt(message):
+    fullcmd = "python " + TXT2IMG + PROMPTARG + "\"" + message.text + "\" " + OTHERARG
+    print(fullcmd)
+    os.system(fullcmd)
+
+@bot.message_handler()
 def hello(message):
-    bot.send_photo(message.chat.id, photo=open('../stable-diffusion/outputs/txt2img-samples/samples/00000.png','rb'))
+    send_prompt(message)
+    list_of_files = glob.glob('../stable-diffusion/outputs/txt2img-samples/samples/*')
+    latest_file = max(list_of_files, key=os.path.getctime)
+    bot.send_photo(message.chat.id, photo=open(latest_file,'rb'))
 
 bot.polling()
