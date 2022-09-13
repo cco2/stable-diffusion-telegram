1aff476b2edffb1e00154189b398f27c38c8c8b1
Chris <pickles133@gmail.com>
2022-09-12 23:04:59 -0700

    Add seed value to picture

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 import glob
+import subprocess
+import random
 import telebot
 from dotenv import load_dotenv
 
 TXT2IMG = "../stable-diffusion/scripts/txt2img.py "
 PROMPTARG = "--prompt "
-OTHERARG = "--plms --n_iter 1 --n_samples 1 --skip_grid --seed -1"
+OTHERARG = "--plms --n_iter 1 --n_samples 1 --skip_grid "
+SEEDARG = "--seed "
 
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
@@ -16,15 +19,21 @@ def greet(message):
     bot.reply_to(message, "Hey! How's it going?")
 
 def send_prompt(message):
-    fullcmd = "python " + TXT2IMG + PROMPTARG + "\"" + message.text + "\" " + OTHERARG
+    seed = random.randint(0,4294967295)
+    fullcmd = "python " + TXT2IMG + PROMPTARG + "\"" + message.text + "\" " + OTHERARG + SEEDARG + str(seed)
     print(fullcmd)
-    os.system(fullcmd)
+    returned_output = subprocess.check_output(fullcmd)
+    print("========")
+    print(returned_output.decode())
+    print("+++++++")
+    return seed
 
 @bot.message_handler()
 def hello(message):
-    send_prompt(message)
+    seed = send_prompt(message)
     list_of_files = glob.glob('../stable-diffusion/outputs/txt2img-samples/samples/*')
     latest_file = max(list_of_files, key=os.path.getctime)
-    bot.send_photo(message.chat.id, photo=open(latest_file,'rb'))
+    #bot.send_photo(message.chat.id, photo=open(latest_file,'rb'))
+    bot.send_photo(message.chat.id, photo=open(latest_file,'rb'), caption="\n seed: {}".format(seed))
 
 bot.polling()
